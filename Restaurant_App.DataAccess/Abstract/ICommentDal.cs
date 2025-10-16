561246ed927741d5ce4c16014e4997389cd5c0af
hazal ilik <ilikhazal16@gmail.com>
2025-10-16 11:39:54 +0300

    Created Interfaces with Repository Pattern in Data Access Layer

---
+++ Restaurant_App.DataAccess/Abstract/ICommentDal.cs
@@ -0,0 +1,17 @@
+﻿using Restaurant_App.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant_App.DataAccess.Abstract
+{
+    public interface ICommentDal: IRepository<Comment>
+    {
+        List<Comment> GetCommentsByProductId(int productId);
+        List<Comment> GetCommentsByUserId(string userId);
+        List<Comment> GetCommentsWithRatingsByProductId(int productId);
+        
+    }
+}
---
+++ Restaurant_App.DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,20 @@
+﻿using Restaurant_App.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant_App.DataAccess.Abstract
+{
+    public interface IProductDal: IRepository<Product>
+    {
+        List<Product> GetTopRatedProducts(Rating rating, int count);
+        List<Product> GetMostPopularProducts(Rating rating, int count);
+        List<Product> SearchProducts(string searchTerm);
+        int GetCountByCategory(string category);
+        Product GetProductDetails(int id);
+        List<Product> GetProductsByCategory(string category, int page, int pageSize);
+        void Update(Product entity, int[] categoryIds);
+    }
+}
