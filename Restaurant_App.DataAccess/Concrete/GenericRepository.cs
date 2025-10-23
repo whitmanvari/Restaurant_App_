22388c6780d29b50fe014f6382a0a2df9bf06511
hazal ilik <ilikhazal16@gmail.com>
2025-10-23 21:17:06 +0300

    Created ITableService and TableManager in Business layer.

--- Restaurant_App.DataAccess/Concrete/GenericRepository.cs
+++ Restaurant_App.DataAccess/Concrete/GenericRepository.cs
@@ -1,11 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Restaurant_App.DataAccess.Abstract;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Restaurant_App.DataAccess.Concrete
 {
