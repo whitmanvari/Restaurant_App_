86454287b40620d9aa352e4ba9b9f7e5d09ca8a6
hazal ilik <ilikhazal16@gmail.com>
2025-10-23 17:49:57 +0300

    Created OrderInRestaurant manager and service both in Business Layer and used dependency injection to use DAL methods.

---
+++ Restaurant_App.Business/Restaurant_App.Business/Abstract/IOrderInRestaurantService.cs
@@ -0,0 +1,21 @@
+﻿using Restaurant_App.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant_App.Business.Abstract
+{
+    public interface IOrderInRestaurantService
+    {
+        Task<List<OrderInRestaurant>> GetOrdersWithDetails();
+        Task<OrderInRestaurant?> GetOrderWithDetails(int orderInRestaurantId);
+        Task<List<OrderInRestaurant>> GetOrdersByTableId(int tableId);
+        Task<List<OrderInRestaurant>> GetOrdersByStatus(OrderStatusInRestaurant status);
+        Task UpdateOrderStatus(int orderInRestaurantId, OrderStatusInRestaurant status);
+        Task AddOrderItem(int orderInRestaurantId, OrderItemInRestaurant orderItem);
+        Task RemoveOrderItem(int orderInRestaurantId, int orderItemId);
+        Task ClearOrderItems(int orderInRestaurantId);
+    }
+}
---
+++ Restaurant_App.Business/Restaurant_App.Business/Concrete/OrderInRestaurantManager.cs
@@ -0,0 +1,60 @@
+﻿using Restaurant_App.Business.Abstract;
+using Restaurant_App.DataAccess.Abstract;
+using Restaurant_App.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant_App.Business.Concrete
+{
+    public class OrderInRestaurantManager: IOrderInRestaurantService
+    {
+        private readonly IOrderInRestaurantDal _orderInRestaurantDal;
+        public OrderInRestaurantManager(IOrderInRestaurantDal orderInRestaurantDal)
+        {
+            _orderInRestaurantDal = orderInRestaurantDal;
+        }
+
+        public async Task AddOrderItem(int orderInRestaurantId, OrderItemInRestaurant orderItem)
+        {
+            await _orderInRestaurantDal.AddOrderItem(orderInRestaurantId, orderItem);
+        }
+
+        public async Task ClearOrderItems(int orderInRestaurantId)
+        {
+            await _orderInRestaurantDal.ClearOrderItems(orderInRestaurantId);
+        }
+
+        public async Task<List<OrderInRestaurant>> GetOrdersByStatus(OrderStatusInRestaurant status)
+        {
+            return await _orderInRestaurantDal.GetOrdersByStatus(status);
+        }
+
+        public async Task<List<OrderInRestaurant>> GetOrdersByTableId(int tableId)
+        {
+            return await _orderInRestaurantDal.GetOrdersByTableId(tableId);
+        }
+
+        public async Task<List<OrderInRestaurant>> GetOrdersWithDetails()
+        {
+            return await _orderInRestaurantDal.GetOrdersWithDetails();
+        }
+
+        public async Task<OrderInRestaurant?> GetOrderWithDetails(int orderInRestaurantId)
+        {
+            return await _orderInRestaurantDal.GetOrderWithDetails(orderInRestaurantId);
+        }
+
+        public async Task RemoveOrderItem(int orderInRestaurantId, int orderItemId)
+        {
+            await _orderInRestaurantDal.RemoveOrderItem(orderInRestaurantId, orderItemId);
+        }
+
+        public async Task UpdateOrderStatus(int orderInRestaurantId, OrderStatusInRestaurant status)
+        {
+            await _orderInRestaurantDal.UpdateOrderStatus(orderInRestaurantId, status);
+        }
+    }
+}
