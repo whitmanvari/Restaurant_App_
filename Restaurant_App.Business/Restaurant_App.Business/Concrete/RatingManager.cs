f0b435de36b8d340727de18d2ce26cf4a20436a8
hazal ilik <ilikhazal16@gmail.com>
2025-10-23 17:37:12 +0300

    Arranged IRatingService and RatingManager in Business Layer.

---
+++ Restaurant_App.Business/Restaurant_App.Business/Concrete/RatingManager.cs
@@ -0,0 +1,70 @@
+﻿using Restaurant_App.Business.Abstract;
+using Restaurant_App.DataAccess.Abstract;
+using Restaurant_App.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant_App.Business.Concrete
+{
+    public class RatingManager : IRatingService
+    {
+        private readonly IRatingDal _ratingDal;
+        public RatingManager(IRatingDal ratingDal)
+        {
+            _ratingDal = ratingDal;
+        }
+        public async Task CreateRating(Rating rating)
+        {
+            await _ratingDal.Create(rating);
+        }
+
+        public async Task DeleteRating(Rating rating)
+        {
+            await _ratingDal.Delete(rating);
+        }
+
+        public async Task<List<Rating>> GetAllRatings(Expression<Func<Rating, bool>>? filter = null)
+        {
+            return await _ratingDal.GetAll(filter);
+        }
+
+        public async Task<double> GetAverageRatingForProduct(int productId)
+        {
+            return await _ratingDal.GetAverageRatingForProduct(productId);
+        }
+
+        public async Task<Rating> GetByRatingId(int ratingId)
+        {
+            return await _ratingDal.GetById(ratingId);
+        }
+
+        public async Task<Rating> GetOneRating(Expression<Func<Rating, bool>>? filter = null)
+        {
+            return await _ratingDal.GetOne(filter);
+        }
+
+        public async Task<List<Rating>> GetRatingsByUserId(string userId)
+        {
+            return await _ratingDal.GetRatingsByUserId(userId);
+        }
+
+        public async Task<List<Rating>> GetRatingsWithComments(int commentId)
+        {
+            return await _ratingDal.GetRatingsWithComments(commentId);
+        }
+
+        public async Task<List<Rating>> GetRatingsWithProducts(int productId)
+        {
+            return await _ratingDal.GetRatingsWithProducts(productId);
+        }
+
+        public async Task UpdateRating(Rating rating)
+        {
+            await _ratingDal.Update(rating);
+        }
+    }
+}
