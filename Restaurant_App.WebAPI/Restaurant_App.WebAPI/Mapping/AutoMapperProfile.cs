add910c056a90608a0b3b33a0c2c38a5d9d6002a
hazal ilik <ilikhazal16@gmail.com>
2025-10-30 18:51:17 +0300

    Auto mapper has been added.

---
+++ Restaurant_App.WebAPI/Restaurant_App.WebAPI/Mapping/AutoMapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using Restaurant_App.Entities.Concrete;
+using Restaurant_App.WebAPI.Dto;
+using AutoMapper;
+
+namespace Restaurant_App.WebAPI.Mapping
+{
+    public class AutoMapperProfile: Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<Category, CategoryDTO>().ReverseMap();
+        }
+    }
+}
