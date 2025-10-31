a483fd439715603208160394cfb76c03c2689fbd
hazal ilik <ilikhazal16@gmail.com>
2025-10-31 12:20:43 +0300

    Auth Controller has been arranged.

--- Restaurant_App.Business/Restaurant_App.Business/Identity/ApplicationIdentityDbContext.cs
+++ Restaurant_App.Business/Restaurant_App.Business/Identity/ApplicationIdentityDbContext.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace Restaurant_App.WebAPI.Identity
+namespace Restaurant_App.Business.Identity
 {
     public class ApplicationIdentityDbContext: IdentityDbContext<ApplicationUser>
     {
--- Restaurant_App.WebAPI/Restaurant_App.WebAPI/Controllers/AuthController.cs
+++ Restaurant_App.WebAPI/Restaurant_App.WebAPI/Controllers/AuthController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Restaurant_App.Business.Abstract;
+using Restaurant_App.Entities.Dto;
 
 namespace Restaurant_App.WebAPI.Controllers
 {
@@ -7,5 +9,38 @@ namespace Restaurant_App.WebAPI.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
+        private readonly IAuthService _authService;
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        //post api/auth/register
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] UserRegisterDTO model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var result = await _authService.Register(model);
+            if (!result)
+                return BadRequest("Kayıt sırasında bir hata oluştu.");
+
+            return Ok("Kullanıcı başarıyla oluşturuldu.");
+        }
+
+        //post api/auth/login
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] UserLoginDTO model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var token = await _authService.Login(model);
+            if (token == null)
+                return Unauthorized("Email veya şifre hatalı.");
+
+            return Ok(new { Token = token });
+        }
     }
 }
