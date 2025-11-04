7af2e968fc571272621eecf588dc0af68283b726
hazal ilik <ilikhazal16@gmail.com>
2025-11-04 20:37:40 +0300

    Arranged AutoMapperProfile.cs

--- Restaurant_App.Application/Mapping/AutoMapperProfile.cs
+++ Restaurant_App.Application/Mapping/AutoMapperProfile.cs
@@ -1,7 +1,6 @@
 ﻿using AutoMapper;
 using Restaurant_App.Application.Dto;
 using Restaurant_App.Entities.Concrete;
-using Restaurant_App.Entities.Enum;
 using Restaurant_App.Entities.Identity;
 
 namespace Restaurant_App.Application.Mapping
@@ -10,87 +9,76 @@ namespace Restaurant_App.Application.Mapping
     {
         public AutoMapperProfile()
         {
-            // Category Mapping
-            CreateMap<Category, CategoryDTO>()
-                .ForMember(dest => dest.Products, opt => opt.MapFrom(src => src.Products));
-            CreateMap<CategoryDTO, Category>()
-                .ForMember(dest => dest.Products, opt => opt.MapFrom(src => src.Products));
+            // Category mapping
+            CreateMap<Category, CategoryDTO>().ReverseMap();
 
-            // Product Mapping
+            // Product mapping
             CreateMap<Product, ProductDTO>()
                 .ForMember(dest => dest.ImageUrls, opt => opt.MapFrom(src => src.Images.Select(i => i.ImageUrl)))
-                .ForMember(dest => dest.CategoryName, opt => opt.MapFrom(src => src.Category.Name));
+                .ForMember(dest => dest.CategoryName, opt => opt.MapFrom(src => (src.Category != null) ? src.Category.Name : string.Empty));
 
-            
             CreateMap<ProductDTO, Product>()
                 .ForMember(dest => dest.Images, opt => opt.MapFrom(src => src.ImageUrls.Select(url => new Image { ImageUrl = url })))
-                .ForMember(dest => dest.Name, opt => opt.Ignore());
+                .ForMember(dest => dest.Category, opt => opt.Ignore());
 
-            // User Mapping
+            // User mapping
             CreateMap<ApplicationUser, UserRegisterDTO>()
                 .ReverseMap()
                 .AfterMap((dto, user) =>
                 {
                     user.Email = dto.Email;
-                    user.UserName = dto.Email;
+                    user.UserName = dto.Email; // Identity için UserName genelde Email ile aynı yapılır.
                     user.FullName = dto.FullName;
                 });
 
-            // Cart Mapping
+            // Cart mapping
             CreateMap<Cart, CartDTO>()
                 .ForMember(dest => dest.Items, opt => opt.MapFrom(src => src.CartItems));
             CreateMap<CartDTO, Cart>()
                 .ForMember(dest => dest.CartItems, opt => opt.MapFrom(src => src.Items));
 
+            // CartItem mapping
             CreateMap<CartItem, CartItemDTO>()
-                .ForMember(dest => dest.ProductName, opt => opt.MapFrom(src => src.Product.Name));
+                .ForMember(dest => dest.ProductName, opt => opt.MapFrom(src => (src.Product != null) ? src.Product.Name : string.Empty));
             CreateMap<CartItemDTO, CartItem>()
-                .ForMember(dest => dest.ProductName, opt => opt.Ignore())
                 .ForMember(dest => dest.Product, opt => opt.Ignore());
 
-            // Reservation Mapping
-            CreateMap<Reservation, ReservationDTO>();
-            CreateMap<ReservationDTO, Reservation>();
+            // Reservation mapping
+            CreateMap<Reservation, ReservationDTO>().ReverseMap();
 
-            CreateMap<Rating, RatingDTO>();
-            CreateMap<RatingDTO, Rating>();
+            // Rating mapping
+            CreateMap<Rating, RatingDTO>().ReverseMap();
 
-            // Comment Mapping
-            CreateMap<Comment, CommentDTO>();
-            CreateMap<CommentDTO, Comment>();
+            // Comment mapping
+            CreateMap<Comment, CommentDTO>().ReverseMap();
 
-            // Table Mapping
-            CreateMap<Table, TableDTO>();
-            CreateMap<TableDTO, Table>();
+            // Table mapping
+            CreateMap<Table, TableDTO>().ReverseMap();
 
-            // Order Mapping
+            // Order mapping
             CreateMap<Order, OrderDTO>()
                 .ForMember(dest => dest.Items, opt => opt.MapFrom(src => src.OrderItems));
             CreateMap<OrderDTO, Order>()
                 .ForMember(dest => dest.OrderItems, opt => opt.MapFrom(src => src.Items));
 
-            // OrderInRestaurant Mapping
+            // OrderInRestaurant mapping
             CreateMap<OrderInRestaurant, OrderInRestaurantDTO>()
-                .ForMember(dest => dest.TableNumber, opt => opt.MapFrom(src => src.Table.TableNumber))
-                .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status.ToString()));
-            CreateMap<OrderInRestaurantDTO, OrderInRestaurant>()
-                .ForMember(dest => dest.Table, opt => opt.Ignore())
-                .ForMember(dest => dest.TableId, opt => opt.MapFrom(src => src.TableId))
-                .ForMember(dest => dest.Status, opt => opt.MapFrom(src => Enum.Parse<OrderStatusInRestaurant>(src.Status, true)));
+                .ForMember(dest => dest.TableNumber, opt => opt.MapFrom(src => (src.Table != null) ? src.Table.TableNumber : string.Empty))
+                .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status.ToString())); 
 
-            // OrderItem Mapping
+            CreateMap<OrderInRestaurantDTO, OrderInRestaurant>()
+                .ForMember(dest => dest.Table, opt => opt.Ignore());
+
+            // OrderItem mapping
             CreateMap<OrderItem, OrderItemDTO>()
-                .ForMember(dest => dest.ProductName, opt => opt.MapFrom(src => src.Product.Name));
+                .ForMember(dest => dest.ProductName, opt => opt.MapFrom(src => (src.Product != null) ? src.Product.Name : string.Empty));
             CreateMap<OrderItemDTO, OrderItem>()
-                .ForMember(dest => dest.ProductName, opt => opt.Ignore())
                 .ForMember(dest => dest.Product, opt => opt.Ignore());
 
-            // OrderItemInRestaurant Mapping
+            // OrderItemInRestaurant mapping
             CreateMap<OrderItemInRestaurant, OrderItemInRestaurantDTO>()
                 .ForMember(dest => dest.ProductName, opt => opt.MapFrom(src => (src.Product != null) ? src.Product.Name : string.Empty));
             CreateMap<OrderItemInRestaurantDTO, OrderItemInRestaurant>()
-                .ForMember(dest => dest.ProductName, opt => opt.Ignore())
                 .ForMember(dest => dest.Product, opt => opt.Ignore());
         }
     }
-}
