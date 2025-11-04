c8fab07d32f9f2b8f2e57137465039798780a677
hazal ilik <ilikhazal16@gmail.com>
2025-11-04 20:38:40 +0300

    Dto's has been updated.

--- Restaurant_App.Application/Dto/RatingDTO.cs
+++ Restaurant_App.Application/Dto/RatingDTO.cs
@@ -5,8 +5,6 @@ namespace Restaurant_App.Application.Dto
     public class RatingDTO: BaseDTO
     {
         public int ProductId { get; set; }
-        public decimal AverageRating { get; set; }
-        public int TotalRatings { get; set; }
         public RatingValue Value { get; set; } = RatingValue.Zero;
         public string UserId { get; set; } = string.Empty;
         public string Comment { get; set; } = string.Empty;
