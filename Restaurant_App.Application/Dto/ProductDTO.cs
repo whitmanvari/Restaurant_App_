2edb11cad27fcc0e78e0207ff898ce27c1cc6977
hazal ilik <ilikhazal16@gmail.com>
2025-11-04 20:38:40 +0300

    Dto's has been updated.

--- Restaurant_App.Application/Dto/ProductDTO.cs
+++ Restaurant_App.Application/Dto/ProductDTO.cs
@@ -7,6 +7,8 @@ namespace Restaurant_App.Application.Dto
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public Allergic Allergic { get; set; } = Allergic.None;
+        public double AverageRating { get; set; }
+        public int TotalRatings { get; set; }
         public decimal Price { get; set; }
         public int CategoryId { get; set; }
         public List<string> ImageUrls { get; set; } = new List<string>();
