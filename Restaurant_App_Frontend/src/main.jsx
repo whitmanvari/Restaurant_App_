486db285f0202abd083712ddcbbf82e734e322cb
hazal ilik <ilikhazal16@gmail.com>
2025-11-06 16:50:51 +0300

    Login Page has been updated.

--- Restaurant_App_Frontend/src/main.jsx
+++ Restaurant_App_Frontend/src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import {Provider} from 'react-redux';
 import {store} from './store/store.js';
+import {BrowserRouter} from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <Provider store ={store}>
-       <App />
+    <BrowserRouter>  
+      <App />
+    </BrowserRouter>
   </Provider>
 )
---
+++ Restaurant_App_Frontend/src/store/slices/authSlice.js
@@ -0,0 +1,72 @@
+import {createSlice, createAsyncThunk} from '@reduxjs/toolkit';
+import api from '../../api/axiosInstance';
+
+const token = localStorage.getItem('token');
+
+const initialState = {
+    token: token || null,
+    isAuthenticated: token ? true : false,
+    status: 'idle',
+    error: null
+};
+
+//asenkron api isteği login
+export const loginUser = createAsyncThunk(
+    'auth/loginUser', //reduxta görünecek eylem adı
+    async (loginData, {rejectWithValue}) => {
+        try{
+            const response = await api.post('Auth/login', loginData);
+            const{token} = response.data;
+            localStorage.setItem('token', token);
+            return token;
+        }
+        catch(error) {
+            return rejectWithValue(error.response.data);
+        }
+    }
+);
+
+//asenkron api isteği: register için
+export const registerUser = createAsyncThunk(
+    'auth/registerUser',
+    async(registerData, {rejectWithValue}) => {
+        try{
+            await api.post('/Auth/register'), registerData;
+            return;
+        }
+        catch(error) {
+            return rejectWithValue(error.response.data);
+        }
+    }
+);
+
+//Auth slice
+//token, isauthanticated vb. güncelleyen kuralları içerir
+export const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    //senkron eylemler
+    reducers: {
+        logout: (state) => {
+            localStorage.removeItem('token');
+            state.token = null;
+            state.isAuthenticated= false;
+        },
+    },
+    extraReducers: (builder) => {
+        builder
+        .addCase(loginUser.pending, (state) => {
+            state.status = 'loading';
+        })
+        .addCase(loginUser.fulfilled, (state, action) => {
+            state.status = 'failed';
+            state.error = action.payload; //hata mesajı
+            state.token = null;
+            state.isAuthenticated = false;
+        });
+    },
+});
+// logout eylemini dışa aktar (componentlerde kullanmak için)
+export const {logout} = authSlice.actions;
+// reducer'ı dışa aktar (store'da kullanmak için)
+export default authSlice.reducer;
