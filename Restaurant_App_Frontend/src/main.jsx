377bc744b711711100c3cf60679ae2ee04190ce9
hazal ilik <ilikhazal16@gmail.com>
2025-11-06 15:09:43 +0300

    Added provider and store.js to the app.

--- Restaurant_App_Frontend/src/main.jsx
+++ Restaurant_App_Frontend/src/main.jsx
@@ -1,7 +1,11 @@
 import './styles/main.scss';
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import {Provider} from 'react-redux';
+import {store} from './store/store.js';
 
 createRoot(document.getElementById('root')).render(
-    <App />
+  <Provider store ={store}>
+       <App />
+  </Provider>
 )
