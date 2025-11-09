439c3a802e62acbcb354da76572828a45f971b92
hazal ilik <ilikhazal16@gmail.com>
2025-11-09 14:26:25 +0300

    Auth Slice has been updated, login page created.

--- Restaurant_App_Frontend/src/store/slices/authSlice.js
+++ Restaurant_App_Frontend/src/store/slices/authSlice.js
@@ -15,17 +15,17 @@ export const loginUser = createAsyncThunk(
     'auth/loginUser', //reduxta görünecek eylem adı
     async (loginData, {rejectWithValue}) => {
         try{
-            const response = await api.post('Auth/login', loginData);
+            const response = await api.post('/Auth/login', loginData);
             const{token} = response.data;
             localStorage.setItem('token', token);
             return token;
         }
         catch(error) {
-            return rejectWithValue(error.response.data);
+            return rejectWithValue(error.response.data) || 'Giriş başarısız!';
         }
     }
 );
-
+//Register
 //asenkron api isteği: register için
 export const registerUser = createAsyncThunk(
     'auth/registerUser',
@@ -35,7 +35,7 @@ export const registerUser = createAsyncThunk(
             return;
         }
         catch(error) {
-            return rejectWithValue(error.response.data);
+            return rejectWithValue(error.response.data) || 'Kayıt başarısız!';
         }
     }
 );
@@ -59,6 +59,12 @@ export const authSlice = createSlice({
             state.status = 'loading';
         })
         .addCase(loginUser.fulfilled, (state, action) => {
+            localStorage.setItem('token', action.payload);
+            state.status='succeeded';
+            state.token = action.payload;
+            state.isAuthenticated= true;
+        })
+        .addCase(loginUser.rejected, (state, action) => {
             state.status = 'failed';
             state.error = action.payload; //hata mesajı
             state.token = null;
