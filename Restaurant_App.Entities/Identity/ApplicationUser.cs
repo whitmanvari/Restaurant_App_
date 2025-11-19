1543b9eded714d5ce301d8197fe2c10547f5e04f
hazal ilik <ilikhazal16@gmail.com>
2025-11-19 08:31:23 +0300

    Updated UserDto due to register page noncompulsory areas.

--- Restaurant_App.Entities/Identity/ApplicationUser.cs
+++ Restaurant_App.Entities/Identity/ApplicationUser.cs
@@ -5,5 +5,7 @@ namespace Restaurant_App.Entities.Identity
     public class ApplicationUser: IdentityUser
     {
         public string FullName { get; set; } = string.Empty;
+        public string? Address { get; set; }
+        public string? City { get; set; }
     }
 }
