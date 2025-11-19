17b5372456a7f054183df61a381efe4a12b4b1a1
hazal ilik <ilikhazal16@gmail.com>
2025-11-19 17:05:20 +0300

    Allergens has been updated

---
+++ Restaurant_App_Frontend/src/constants/allergens.js
@@ -0,0 +1,12 @@
+export const Allergens = [
+    { id: 1, label: "Gluten", icon: "fas fa-bread-slice" },
+    { id: 2, label: "Kuruyemiş (Fındık/Ceviz)", icon: "fas fa-cookie" },
+    { id: 4, label: "Süt Ürünleri", icon: "fas fa-cheese" },
+    { id: 8, label: "Soya", icon: "fas fa-seedling" },
+    { id: 16, label: "Yumurta", icon: "fas fa-egg" },
+    { id: 32, label: "Kabuklu Deniz Ürünleri", icon: "fas fa-shrimp" },
+    { id: 64, label: "Balık", icon: "fas fa-fish" },
+    { id: 128, label: "Yer Fıstığı", icon: "fas fa-genderless" },
+    { id: 512, label: "Buğday", icon: "fas fa-wheat" },
+    { id: 1024, label: "Susam", icon: "fas fa-cookie-bite" }
+];
